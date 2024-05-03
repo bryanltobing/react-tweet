259865655b933f7b6e97bcbad4700ba12df45c86
bryanltobing <bryantobing0@gmail.com>
2024-05-03 11:59:07 +0800

    feat: configure sveltekit-app to support turbo scripts command

--- apps/sveltekit-app/vite.config.ts
+++ apps/sveltekit-app/vite.config.ts
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [sveltekit()],
+  server: {
+    port: 5432,
+  },
+  preview: {
+    port: 4321,
+  },
+})
